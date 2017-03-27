0659da33c1a11ad7a9dfd9950f61c4087048f5fd
Ravindra Prasad <ravindraprasad@yahoo.com>
2017-03-27 23:36:11 +0530

    Update index.py

--- index.py
+++ index.py
@@ -217,5 +217,9 @@ def webhook():
         return "Wrong Verify Token"
     return "Nothing"
 
+def log(message):  # simple wrapper for logging to stdout on heroku
+    print str(message)
+    sys.stdout.flush()
+    
 if __name__ == '__main__':
     app.run(debug=True)
